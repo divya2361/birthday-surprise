9dd5c2693beb939f954b08f328445a7efb904008
Divya Modi <divya23modi@gmail.com>
2026-08-28 20:24:13 +0530

    Add birthday surprise website

---
+++ src/main/java/com/birthday/surprise/BirthdaySurpriseApplication.java
@@ -0,0 +1,13 @@
+package com.birthday.surprise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirthdaySurpriseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirthdaySurpriseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/birthday/surprise/BirthdaySurpriseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.birthday.surprise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirthdaySurpriseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
